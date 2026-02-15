3c349149a25dd1b0d13c5585f81165fc20739005
vinudasenith <ha.vinudas@gmail.com>
2026-02-15 10:00:22 +0530

    feat: add SplashScreen component

---
+++ app/splashScreen.tsx
@@ -0,0 +1,31 @@
+// splash.tsx
+import { StatusBar, Text, View } from 'react-native';
+
+export default function SplashScreen() {
+    return (
+        <View className="flex-1 bg-slate-50 items-center justify-center">
+
+            <StatusBar barStyle="dark-content" />
+
+            {/* Logo */}
+            <View className="items-center">
+
+                {/* App Name */}
+                <Text className="text-slate-900 text-5xl font-bold tracking-tight">
+                    Vision
+                    <Text className="text-blue-500">Assist</Text>
+                </Text>
+
+                {/* Divider line */}
+                <View className="w-12 h-0.5 bg-blue-500 mt-5 mb-5" />
+
+                {/* Tagline */}
+                <Text className="text-slate-400 text-xs font-semibold tracking-[4px] uppercase">
+                    Smart Vision. Smart Living.
+                </Text>
+
+            </View>
+
+        </View>
+    );
+}
