dcc3b4fcebba6fa5bd413150f0ca1d92b699e536
vinudasenith <ha.vinudas@gmail.com>
2026-02-17 04:41:39 +0530

    feat: add route-based BottomNav visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,22 @@
-import { Stack } from 'expo-router';
+import { Stack, usePathname } from 'expo-router';
 import { View } from 'react-native';
+import BottomNav from '../components/BottomNav';
 import "../global.css"; // Keep your global styles
 
 export default function Layout() {
+  const pathname = usePathname();
+
+  //  BottomNav should not appear
+  const hideBottomNav = ['/login', '/register', '/welcomeScreen'];
+
   return (
     <View style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
         <Stack screenOptions={{ headerShown: false }} />
       </View>
+
+      {/* Only render BottomNav on non-auth pages */}
+      {!hideBottomNav.includes(pathname) && <BottomNav />}
     </View>
   );
 }
