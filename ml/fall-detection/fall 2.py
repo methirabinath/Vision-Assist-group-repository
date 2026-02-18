c3a170f7a32d71befebe033421b8a0ebe207f2bb
Sinethma Pathirage <sinethmapathirage@gmail.com>
2026-02-18 14:35:56 +0530

    git commit -am Added accelerometer and gyroscope data processing functions

--- ml/fall-detection/fall 2.py
+++ ml/fall-detection/fall 2.py
@@ -18,7 +18,7 @@ if HARDWARE_AVAILABLE:
     bus.write_byte_data(MPU_ADDR, PWR_MGMT_1, 0)
 
 
-
+print("IMU Module Started...")
 
 
 
@@ -35,18 +35,18 @@ def read_raw_data(addr):
 
     return value
 
+def get_acceleration():
+    ax = read_raw_data(ACCEL_XOUT_H) / 16384.0
+    ay = read_raw_data(ACCEL_XOUT_H + 2) / 16384.0
+    az = read_raw_data(ACCEL_XOUT_H + 4) / 16384.0
+    return ax, ay, az
 
-while True:
-    ax, ay, az = get_acceleration()
-    gx, gy, gz = get_gyroscope()
+def get_gyroscope():
+    gx = read_raw_data(GYRO_XOUT_H) / 131.0
+    gy = read_raw_data(GYRO_XOUT_H + 2) / 131.0
+    gz = read_raw_data(GYRO_XOUT_H + 4) / 131.0
+    return gx, gy, gz
 
-    print(f"Ax:{ax:.2f} Ay:{ay:.2f} Az:{az:.2f} | GyroZ:{gz:.2f}")
-    time.sleep(0.5)
 
-
-def calculate_tilt(ax, ay, az):
-    roll = math.atan2(ay, az) * 57.3
-    pitch = math.atan2(-ax, math.sqrt(ay * ay + az * az)) * 57.3
-    return roll, pitch
 
 
