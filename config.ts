620d994678e6c1d8a5386935de0f90834b371fb6
vinudasenith <ha.vinudas@gmail.com>
2026-03-29 05:24:29 +0530

    chore: add backend hosted url

--- config.ts
+++ config.ts
@@ -1,1 +1,6 @@
+// export const BASE_URL: string = "https://visionassist-backend.onrender.com";
+// export const BASE_URL: string = "http://192.168.8.100:5000";
+
 export const BASE_URL: string = "https://visionassist-backend.onrender.com";
+
+// export const BASE_URL: string = "http://192.168.8.100:5000";
