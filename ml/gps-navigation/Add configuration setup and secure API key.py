a0284803e53442457a3dd47519fe5e3215f4bdc1
Dinujaya Pathirage <pathiragedinujaya4308@gmail.com>
2026-03-30 00:12:08 +0530

    Update real API Key.py

--- ml/gps-navigation/Add configuration setup and secure API key.py
+++ ml/gps-navigation/Add configuration setup and secure API key.py
@@ -1,6 +1,6 @@
 import os
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_API_KEY = os.getenv("AIzaSyB-EVy-UqPXRYaBR7EaM_w0iGS6klxOJoQ")
 
 if not GOOGLE_API_KEY:
     raise RuntimeError("GOOGLE_API_KEY missing")
