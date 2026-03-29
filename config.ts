3f00d641b3da42786b9be59e56e7a4c7f0c0deca
vinudasenith <ha.vinudas@gmail.com>
2026-03-29 22:57:09 +0530

    chore: change base url

--- config.ts
+++ config.ts
@@ -1,1 +1,3 @@
-export const BASE_URL: string = "https://visionassist-backend.onrender.com";
+// export const BASE_URL: string = "https://visionassist-backend.onrender.com";
+
+export const BASE_URL: string = "http://localhost:5000";
